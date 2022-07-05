17bdf629771962dd6695c2a31054af96a50d94fa
talla <btndiaye30@gmail.com>
2022-07-05 13:53:13 +0000

    Correction erreur dans la fonction query

--- pages/home.php
+++ pages/home.php
@@ -7,7 +7,8 @@
   </div> -->
   <div class="container" style="margin-top:100px">
 </ul >
-<?php foreach($db->query('select * from article') as $post):?>
+
+<?php foreach($db->query('select * from article','\App\Table\Article') as $post):?>
           <h2><a href=<?=$post->getUrl()?> > <?= $post -> titre ?></a></h2>
           <p> <?= $post -> titre ?> </p>
 <?php endforeach; ?>
